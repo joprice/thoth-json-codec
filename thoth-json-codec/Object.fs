a2a2b99ee284158c08162861e528e0961d30b010
Joseph Price <pricejosephd@gmail.com>
2024-07-26 13:37:23 -0400

    migrate to thoth core

--- thoth-json-codec/Object.fs
+++ thoth-json-codec/Object.fs
@@ -4,59 +4,49 @@ namespace Thoth.Json.Codec
 namespace Thoth.Json.Net.Codec
 #endif
 
-#if FABLE_COMPILER
-open Thoth.Json
-#else
-open Thoth.Json.Net
-#endif
+open Thoth.Json.Core
+// #if FABLE_COMPILER
+// open Thoth.Json.Core
+// #else
+// open Thoth.Json.Net
+// #endif
 
 [<AutoOpen>]
 module ObjectCodecComputationExpression =
 
-  [<NoComparison>]
-  type ObjectCodecFieldSet<'t, 'u> =
-    {
-      Values : 't -> (string * JsonValue) list
-      Decoder : Decoder<'t>
-      Picker : 'u -> 't
-    }
+    [<NoComparison>]
+    type ObjectCodecFieldSet<'t, 'u> =
+        { Values: 't -> (string * IEncodable) list
+          Decoder: Decoder<'t>
+          Picker: 'u -> 't }
 
-  module ObjectCodecFieldSet =
+    module ObjectCodecFieldSet =
 
-    let zip (a : ObjectCodecFieldSet<'a, 'u>) (b : ObjectCodecFieldSet<'b, 'u>) : ObjectCodecFieldSet<'a * 'b, 'u> =
-      {
-        Values = fun (i, j) -> a.Values i @ b.Values j
-        Decoder =
-          Decode.map2
-            (fun i j -> i, j)
-            a.Decoder
-            b.Decoder
-        Picker = fun u -> a.Picker u, b.Picker u
-      }
+        let zip (a: ObjectCodecFieldSet<'a, 'u>) (b: ObjectCodecFieldSet<'b, 'u>) : ObjectCodecFieldSet<'a * 'b, 'u> =
+            { Values = fun (i, j) -> a.Values i @ b.Values j
+              Decoder = Decode.map2 (fun i j -> i, j) a.Decoder b.Decoder
+              Picker = fun u -> a.Picker u, b.Picker u }
 
-    let complete (f : 't -> 'u) (m : ObjectCodecFieldSet<'t, 'u>) : Codec<'u> =
-      let codec =
-        Codec.create
-          (fun t -> m.Values t |> Encode.object)
-          m.Decoder
+        let complete (f: 't -> 'u) (m: ObjectCodecFieldSet<'t, 'u>) : Codec<'u> =
+            let codec = Codec.create (fun t -> m.Values t |> Encode.object) m.Decoder
 
-      Codec.map f m.Picker codec
+            Codec.map f m.Picker codec
 
-  type ObjectCodecBuilder() =
-    member this.MergeSources(a : ObjectCodecFieldSet<'a, 'u>, b : ObjectCodecFieldSet<'b, 'u>) : ObjectCodecFieldSet<'a * 'b, 'u> =
-      ObjectCodecFieldSet.zip a b
+    type ObjectCodecBuilder() =
+        member this.MergeSources
+            (a: ObjectCodecFieldSet<'a, 'u>, b: ObjectCodecFieldSet<'b, 'u>)
+            : ObjectCodecFieldSet<'a * 'b, 'u> =
+            ObjectCodecFieldSet.zip a b
 
-    member this.BindReturn(m : ObjectCodecFieldSet<'t, 'u>, f : 't -> 'u) : Codec<'u> =
-      ObjectCodecFieldSet.complete f m
+        member this.BindReturn(m: ObjectCodecFieldSet<'t, 'u>, f: 't -> 'u) : Codec<'u> =
+            ObjectCodecFieldSet.complete f m
 
-  let objectCodec = ObjectCodecBuilder()
+    let objectCodec = ObjectCodecBuilder()
 
-  [<RequireQualifiedAccess>]
-  module Codec =
+    [<RequireQualifiedAccess>]
+    module Codec =
 
-    let field (fieldName : string) (picker : 'u -> 't) (fieldCodec : Codec<'t>): ObjectCodecFieldSet<'t, 'u> =
-      {
-        Values = fun i -> [ fieldName, fieldCodec.Encoder i ]
-        Decoder = Decode.field fieldName fieldCodec.Decoder
-        Picker = picker
-      }
+        let field (fieldName: string) (picker: 'u -> 't) (fieldCodec: Codec<'t>) : ObjectCodecFieldSet<'t, 'u> =
+            { Values = fun i -> [ fieldName, fieldCodec.Encoder i ]
+              Decoder = Decode.field fieldName fieldCodec.Decoder
+              Picker = picker }
--- thoth-json-codec/Variant.fs
+++ thoth-json-codec/Variant.fs
@@ -4,98 +4,82 @@ namespace Thoth.Json.Codec
 namespace Thoth.Json.Net.Codec
 #endif
 
-#if FABLE_COMPILER
-open Thoth.Json
-#else
-open Thoth.Json.Net
-#endif
+open Thoth.Json.Core
+// #if FABLE_COMPILER
+// open Thoth.Json
+// #else
+// open Thoth.Json.Net
+// #endif
 
 [<AutoOpen>]
 module VariantCodecBuilder =
 
-  type VariantEncoding =
-    | TagAndValue of tagName : string * valueName : string
-    | OnTag
+    type VariantEncoding =
+        | TagAndValue of tagName: string * valueName: string
+        | OnTag
 
-  [<NoComparison>]
-  type VariantCase<'t, 'v> =
-    {
-      Value : 't
-      Decoders : Map<string, Decoder<'v>>
-    }
+    [<NoComparison>]
+    type VariantCase<'t, 'v> =
+        { Value: 't
+          Decoders: Map<string, Decoder<'v>> }
 
-  module VariantCase =
+    module VariantCase =
 
-    let zip (a : VariantCase<'a, 'v>) (b : VariantCase<'b, 'v>) : VariantCase<'a * 'b, 'v> =
-      {
-        Value = a.Value, b.Value
-        Decoders =
-          Map.ofSeq
-            [
-              yield! Map.toSeq a.Decoders
-              yield! Map.toSeq b.Decoders
-            ]
-      }
+        let zip (a: VariantCase<'a, 'v>) (b: VariantCase<'b, 'v>) : VariantCase<'a * 'b, 'v> =
+            { Value = a.Value, b.Value
+              Decoders = Map.ofSeq [ yield! Map.toSeq a.Decoders; yield! Map.toSeq b.Decoders ] }
 
-    let complete (variantEncoding : VariantEncoding) (f : 't -> ('v -> VariantEncoding -> JsonValue)) (x : VariantCase<'t, 'v>) : Codec<'v> =
-      let decodeForTag tag fieldName : Decoder<_> =
-        match Map.tryFind tag x.Decoders with
-        | Some decoder ->
-          Decode.field fieldName decoder
-        | None ->
-          Decode.fail $"The tag \"{tag}\" was not recognized"
+        let complete
+            (variantEncoding: VariantEncoding)
+            (f: 't -> ('v -> VariantEncoding -> JsonValue))
+            (x: VariantCase<'t, 'v>)
+            : Codec<'v> =
+            let decodeForTag tag fieldName : Decoder<_> =
+                match Map.tryFind tag x.Decoders with
+                | Some decoder -> Decode.field fieldName decoder
+                | None -> Decode.fail $"The tag \"{tag}\" was not recognized"
 
-      Codec.create
-        (fun (v : 'v) -> f x.Value v variantEncoding)
-        (
-          match variantEncoding with
-          | TagAndValue (tagName, valueName) ->
-            Decode.field tagName Decode.string
-            |> Decode.andThen (fun tag -> decodeForTag tag valueName)
-          | OnTag ->
-            Decode.keys
-            |> Decode.andThen
-              (fun keys ->
-                let recognizedKeys =
-                  keys
-                  |> Seq.filter (fun key -> x.Decoders |> Map.containsKey key)
+            Codec.create
+                (fun (v: 'v) -> f x.Value v variantEncoding)
+                (match variantEncoding with
+                 | TagAndValue(tagName, valueName) ->
+                     Decode.field tagName Decode.string
+                     |> Decode.andThen (fun tag -> decodeForTag tag valueName)
+                 | OnTag ->
+                     Decode.keys
+                     |> Decode.andThen (fun keys ->
+                         let recognizedKeys =
+                             keys |> Seq.filter (fun key -> x.Decoders |> Map.containsKey key)
 
-                match Seq.tryExactlyOne recognizedKeys with
-                | Some tag ->
-                  decodeForTag tag tag
-                | None ->
-                  let found =
-                    recognizedKeys
-                    |> Seq.map (fun x -> $"\"{x}\"")
-                    |> String.concat ", "
+                         match Seq.tryExactlyOne recognizedKeys with
+                         | Some tag -> decodeForTag tag tag
+                         | None ->
+                             let found = recognizedKeys |> Seq.map (fun x -> $"\"{x}\"") |> String.concat ", "
 
-                  Decode.fail $"Expected exactly one object key but found: {found}"))
+                             Decode.fail $"Expected exactly one object key but found: {found}"))
 
-  type VariantCodecBuilder(variantEncoding : VariantEncoding) =
-    member this.MergeSources(a, b) =
-      VariantCase.zip a b
+    type VariantCodecBuilder(variantEncoding: VariantEncoding) =
+        member this.MergeSources(a, b) = VariantCase.zip a b
 
-    member this.BindReturn(x, f) =
-      VariantCase.complete variantEncoding f x
+        member this.BindReturn(x, f) =
+            VariantCase.complete variantEncoding f x
 
-  let variantCodecWithEncoding variantEncoding = VariantCodecBuilder(variantEncoding)
+    let variantCodecWithEncoding variantEncoding = VariantCodecBuilder(variantEncoding)
 
-  let variantCodec = VariantCodecBuilder(OnTag)
+    let variantCodec = VariantCodecBuilder(OnTag)
 
-  [<RequireQualifiedAccess>]
-  module Codec =
+    [<RequireQualifiedAccess>]
+    module Codec =
 
-    let case (tag : string) (caseConstructor : 't -> 'v) (caseCodec : Codec<'t>) : VariantCase<'t -> VariantEncoding -> JsonValue, 'v> =
-      {
-        Value =
-          fun t ->
-            function
-            | OnTag -> Encode.object [ tag, caseCodec.Encoder t ]
-            | TagAndValue (tagName, valueName) ->
-              Encode.object
-                [
-                  tagName, Encode.string tag
-                  valueName, caseCodec.Encoder t
-                ]
-        Decoders = Map.ofSeq [ tag, caseCodec.Decoder |> Decode.map caseConstructor ]
-      }
+        let case
+            (tag: string)
+            (caseConstructor: 't -> 'v)
+            (caseCodec: Codec<'t>)
+            : VariantCase<'t -> VariantEncoding -> JsonValue, 'v> =
+            { Value =
+                fun t ->
+                    function
+                    | OnTag -> Encode.object [ tag, caseCodec.Encoder t ]
+                    | TagAndValue(tagName, valueName) ->
+                        Encode.object [ tagName, Encode.string tag; valueName, caseCodec.Encoder t ]
+              Decoders = Map.ofSeq [ tag, caseCodec.Decoder |> Decode.map caseConstructor ] }
